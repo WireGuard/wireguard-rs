2947f02293d1efe06dfe182945aa54c437d9d327
Emīls Piņķis <emils@mullvad.net>
2021-04-22 11:04:54 +0100

    Unify UAPI implementation for unixes
    Signed-off-by: Emīls <emils@mullvad.net>

--- src/platform/linux/mod.rs
+++ src/platform/linux/mod.rs
@@ -1,7 +1,6 @@
 mod tun;
-mod uapi;
 mod udp;
 
+pub use crate::platform::unix::uapi::UnixUAPI as UAPI;
 pub use tun::LinuxTun as Tun;
-pub use uapi::LinuxUAPI as UAPI;
 pub use udp::LinuxUDP as UDP;
